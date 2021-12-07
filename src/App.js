e52700a138f6ea89635271e3a2429de5769d97ff
misun <mcoding777@naver.com>
2021-12-07 19:24:46 +0900

    chart1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 
 function App() {
+
+  function Chart1() {
+    return (
+      <div className="chart1">
+          <div style={{height: 100}}><span>능력<br/>발휘</span></div>
+          <div style={{height: 100}}><span>자율성</span></div>
+          <div style={{height: 100}}><span>보수</span></div>
+          <div style={{height: 100}}><span>안정성</span></div>
+          <div style={{height: 100}}><span>사회적<br/>인정</span></div>
+          <div style={{height: 100}}><span>사회<br/>봉사</span></div>
+          <div style={{height: 100}}><span>자기<br/>계발</span></div>
+          <div style={{height: 100}}><span>창의성</span></div>
+      </div>
+    )
+  }
+  
   return (
-    <div className="App">
-      <h1>Hello, React</h1>
-    </div>
+    <>
+      <header className="App">
+        <h1>Hello, JS Chart</h1>
+      </header>
+      <main>
+        <Chart1 />
+      </main>
+    </>
   );
 }
 
